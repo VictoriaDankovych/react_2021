e1392aee7af8385f1d19cf835f1a991b7069927c
VictoriaDankovych <vicadankovych1999@gmail.com>
2021-05-04 22:47:31 +0300

    homework2

---
+++ src/components/User/User.js
@@ -0,0 +1,14 @@
+export default function User({item}) {
+    return (
+        <div>
+            <h1> {item.id} -{item.name} - {item.age}</h1>
+            <div>
+                city - {item.address.city}
+                <br/>
+                street - {item.address.street}
+                <br/>
+                houseNum -{item.address.number}
+            </div>
+        </div>
+    );
+}
